c8251d59c3947f0accd7a9bea9956a31223d0039
akashtalole <taloleakash@gmail.com>
2022-05-07 10:45:34 +0530

    updated fleet main file

--- datagen/fleet/main.py
+++ datagen/fleet/main.py
@@ -1,30 +1,26 @@
 import logging
-from kaa_client import KaaClient
-from air_station import AirStation
 import time
-import json
+from kaa_client import KaaClient
+from vehicle import Vehicle
 
 # Change application version
 application_version = ''
 # If you revoked the token, update this value with new one
-token = 'air-station-1'
+token = 'vehicle-1'
  
 mqtt_host = 'mqtt.cloud.kaaiot.com'
 mqtt_port = 1883
-
-publish_interval = 60
-
-metadata = {
-  "name": "Newcastle Cradlewell Roadside - UKA00528",
-  "lon": -1.595362,
-  "lat": 54.986405,
-  "location": "Newcastle upon Tyne"
-}
  
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 kaa_client = KaaClient(mqtt_host, mqtt_port, application_version, token)
-air_station = AirStation(kaa_client, metadata)
+vehicle = Vehicle(kaa_client)
+
+# Command handler
+kaa_client.add_command_handler("remote_toggle", vehicle.sensor_toggle)
+
+# Configuration handler
+kaa_client.add_configuration_status_handler(vehicle.configuration_handler)
 
 logging.info(
     f"""
@@ -39,13 +35,12 @@ logging.info(
 # Start mqtt connection
 kaa_client.connect()
 
-logging.info(f"Send device metadata [{air_station.get_device_metadata()}]")
-kaa_client.publish_metadata(air_station.get_device_metadata())
+logging.info(f"Send device metadata [{vehicle.get_device_metadata()}]")
+kaa_client.publish_metadata(vehicle.get_device_metadata())
 
 while True:
-    logging.info("Send Air pollution data")
-    data_sample = air_station.get_data_sample()
-    logging.info(f"{json.dumps(data_sample)}")
+    logging.info("Send Vehicle data")
+    data_sample = vehicle.get_data_sample()
+    logging.info(f"{data_sample}")
     kaa_client.publish_data_collection(data_sample)
-    kaa_client.publish_metadata(air_station.get_latest_metadata())
-    time.sleep(publish_interval)
+    time.sleep(1)
